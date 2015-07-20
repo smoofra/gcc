266c629c3064208b1dc0daadca9e05104c0f7caa
Lawrence D'Anna <larry@elder-gods.org>
2015-07-19 21:10:21 -0700

    libierty: change _ANSIDECL_H to ANSIDECL_H
    ansidecl.h is using _ANSIDECL_H as it's guard #ifdef.  But names that start with
    underscore are reserved, and the latest version of Xcode seems to be exercising
    its prerogative to define _ANSIDECL_H.
    
    This patch changes _ANSIDECL_H to ANSIDECL_H so Xcode can still be used to
    bootstrap gcc.
    
    Signed-off-by: Lawrence D'Anna <larry@elder-gods.org>

--- include/ansidecl.h
+++ include/ansidecl.h
@@ -33,8 +33,8 @@ Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA. 
    wrappers around __attribute__.  Also, __extension__ will be #defined
    to nothing if it doesn't work.  See below.  */
 
-#ifndef	_ANSIDECL_H
-#define _ANSIDECL_H	1
+#ifndef	ANSIDECL_H
+#define ANSIDECL_H	1
 
 #ifdef __cplusplus
 extern "C" {
